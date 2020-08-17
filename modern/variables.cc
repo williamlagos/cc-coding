8fac5bb8d01aeb1222b327d6de951df16b1e3a77
William Oliveira de Lagos <william.lagos@icloud.com>
2020-08-17 19:27:25 -0300

    Added variables testing for modern C++

---
+++ modern/variables.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main(int argc, char** argv) {
+    auto vec = { 1, 2, 3 };
+    auto text = "Hello World!";
+    std::cout << text << std::endl;
+    for(auto v: vec) {
+        std::cout << v << std::endl;
+    }
+    std::tuple<int, int> tup = {1, 2};
+    auto [v1, v2] = tup;
+    std::cout << v1 << v2 << std::endl;
+    return 0;
+}
