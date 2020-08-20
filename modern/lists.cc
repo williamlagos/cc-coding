9e4f612b0acc8079cdb750612e3b7c71adcb43f5
William Oliveira de Lagos <william.lagos@icloud.com>
2020-08-20 14:44:33 -0300

    Added variadic sample to lists

--- modern/lists.cc
+++ modern/lists.cc
@@ -1,5 +1,11 @@
 #include<iostream>
 
+template <typename First, typename... Args>
+auto sum(const First first, const Args... args) -> decltype(first) {
+    const auto values = {first, args...};
+    return std::accumulate(values.begin(), values.end(), First{0});
+}
+
 int main(int argc, char** argv) {
     std::tuple<int, int> tup = {1, 2};
     auto [v1, v2] = tup;
