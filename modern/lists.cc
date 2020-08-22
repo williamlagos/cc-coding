9520c1283bcddf5c2651dce286091fd39468ccb6
William Oliveira de Lagos <william.lagos@icloud.com>
2020-08-22 15:08:44 -0300

    Added tie and array samples

--- modern/lists.cc
+++ modern/lists.cc
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<numeric>
+#include<array>
 
 template <typename First, typename... Args>
 auto sum(const First first, const Args... args) -> decltype(first) {
@@ -9,6 +11,10 @@ auto sum(const First first, const Args... args) -> decltype(first) {
 int main(int argc, char** argv) {
     std::tuple<int, int> tup = {1, 2};
     auto [v1, v2] = tup;
+    int x1, x2;
+    std::tie(x1, x2) = tup;
     std::cout << v1 << v2 << std::endl;
+    std::array<int, 5> ar {1, 2, 3, 4, 5};
+    for (int& x : ar) x *= 2;
     return 0;
 }
